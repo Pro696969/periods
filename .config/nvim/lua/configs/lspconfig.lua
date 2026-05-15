e6686d1de83f086f081d3d5e5cbf4db008d454f7
Pro696969 <krishnakumaarmc@gmail.com>
2026-05-15 15:34:38 +0530

    idk

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -2,7 +2,7 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local nvlsp = require "nvchad.configs.lspconfig"
-local servers = { "html", "cssls", "pyright", "ruff", "clangd"}
+local servers = { "html", "cssls", "pyright", "ruff", "clangd", "jdtls" }
 
 -- lsps with default config
 for _, server in ipairs(servers) do
@@ -22,6 +22,45 @@ vim.lsp.config("pyright", {
         autoSearchPaths = true,
         diagnosticMode = "openFilesOnly",
         useLibraryCodeForTypes = true,
+      },
+    },
+  },
+})
+
+vim.lsp.config("clangd", {
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto", "hpp" },
+})
+
+vim.lsp.config("jdtls", {
+  filetypes = { "java", "groovy" },
+})
+
+vim.lsp.config("jdtls", {
+  filetypes = { "java", "groovy" },
+  cmd = {
+    "/usr/lib/jvm/java-21-openjdk/bin/java", -- JDTLS runs on Java 21
+    "-jar",
+    "/home/pro696969/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.7.100.v20251111-0406.jar",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+  },
+  settings = {
+    java = {
+      configuration = {
+        runtimes = {
+          {
+            name = "JavaSE-11",
+            path = "/usr/lib/jvm/java-11-openjdk/",
+            default = true, -- your projects use Java 11
+          },
+          {
+            name = "JavaSE-21",
+            path = "/usr/lib/jvm/java-21-openjdk/",
+          },
+        },
       },
     },
   },
